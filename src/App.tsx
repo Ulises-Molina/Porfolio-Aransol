f9276562d01e9c7cd10e2c263fd207990e7f6fc2
Ulises Molina <uli.molina10@gmail.com>
2024-12-06 11:03:24 -0300

    Agregue responsive

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,15 @@ export const App: React.FC = () => {
     const currentYear:number = new Date().getFullYear()
     return (
         <>
-        <main className="bg-black h-screen overflow-x-hidden">
-            <section className="bg-red h-[80vh] w-screen flex flex-col justify-center items-center animate-fadeIn delay-500">
+        <main className="bg-black h-screen overflow-x-hidden min-w-[560px]">
+            <section className="bg-red h-[80vh] w-screen flex flex-col justify-center items-center animate-fadeIn delay-500 min-w-full">
                 <img src="/home.png" alt="imagen portada" className="w-full h-full opacity-0 animate-fadeInImage delay-500"></img>
                 <div className="pt-8 opacity-0 animate-slideInUp delay-700">
                     <h1 className="text-white text-7xl mt-6 font-light">PORFOLIO</h1>
                     <h1 className="text-white text-4xl mt-4 font-light">MARISOL ARANCIBIA</h1>
                 </div>
             </section>
-            <div className="flex justify-between pt-8 text-white px-16 text-xl font-light">
+            <div className="hidden lg:flex justify-between items-center gap-5 pt-8 text-white px-16 text-xl font-light">
                 <a href="#first" className="relative group text-white hover:opacity-85 opacity-50 transition-all duration-500">
                     MORFOLOGIA DEL SER
                     <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-white transition-all duration-500 group-hover:w-full"></span>
@@ -25,7 +25,7 @@ export const App: React.FC = () => {
                 </a>
             </div>
         </main>
-        <div className="bg-black relative">
+        <div className="bg-black relative min-w-[560px]">
         <article className="h-screen bg-black">
         <motion.div className="w-full h-full"
             initial= {{opacity: 0,scale : 0.8, translateY: 150}}
@@ -33,16 +33,18 @@ export const App: React.FC = () => {
                 <img src="/second.jpg" alt="segunda imagen" className="w-full h-full object-contain py-16"></img>
         </motion.div>
         </article>
-        <article id="first" className="h-screen bg-black flex justify-center gap-32">
+        <article id="first" className="h-auto bg-black flex flex-col lg:flex-row justify-center items-center gap-5">
         <motion.div
+            className="flex justify-center items-center"
             initial= {{opacity: 0,scale: 0.85,rotate:-30}}
             whileInView={{opacity: 1, scale: 1,transition: {duration: 1.5},rotate:0}}>
-                <img src="/third.webp" alt="tercera imagen" className="w-full h-full object-contain py-10"></img>
+                <img src="/third.webp" alt="tercera imagen" className="h-full object-contain w-3/5"></img>
         </motion.div>
         <motion.div
+            className="flex justify-center items-center"
             initial= {{opacity: 0,scale: 0.85,rotate:30}}
             whileInView={{opacity: 1, scale: 1,transition: {duration: 1.5},rotate:0}}>
-                <img src="/four.webp" alt="cuarta imagen" className="w-full h-full object-contain py-10"></img>
+                <img src="/four.webp" alt="cuarta imagen" className="h-full object-contain w-3/5"></img>
             </motion.div>
             
         </article>
@@ -69,7 +71,7 @@ export const App: React.FC = () => {
             <KeyboardArrowUp fontSize="large"></KeyboardArrowUp>
             </motion.div>
         </div>
-        <footer className="bg-neutral-950 h-20 flex justify-center items-center">
+        <footer className="bg-neutral-950 h-20 flex justify-center items-center min-w-[560px]">
             <h3 className="text-white opacity-50">Hecho por Ulises Molina © {currentYear}</h3>
         </footer>
         </>
